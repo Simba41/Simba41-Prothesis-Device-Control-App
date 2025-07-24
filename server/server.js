9b1b8d39bd2aa15ba84799f9582faedbecdcddca
Simba41 <162508682+Simba41@users.noreply.github.com>
2025-07-24 23:46:16 +0300

    Working on listenig to server and require's

---
+++ server/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const apiRoutes = require('./routes/api');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api', apiRoutes);
+
+
+const path      = require('path');   
+app.use(express.static(path.join(__dirname, '..')));
+
+
+ 
+app.get('/', (_, res) =>
+  res.sendFile(path.join(__dirname, '..', 'index.html'))
+);
+
+
+
+
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port", PORT));
